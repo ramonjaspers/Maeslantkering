c6d49e8cf524fd50879eb3ab20dd2952e358fe8b
Frank <Frank.vanKan@student.hu.nl>
2018-01-29 12:01:18 +0100

    created new functions and altered old ones

--- database.py
+++ database.py
@@ -2,26 +2,29 @@ import mysql.connector
 from mysql.connector import errorcode
 
 """Connection with database (doesnt change beceause of static ip)"""
-connection = mysql.connector.connect(host='192.168.42.2',
+def makeDatabaseConnection():
+    global connection
+    global cursor
+    connection = mysql.connector.connect(host='192.168.42.2',
                                      user="root",
                                      passwd="root",
                                      database='maeslantkering', )
-cursor = connection.cursor(buffered=True, dictionary=True)
-"""try to get 1 result back (not all because of possible giant query)"""
-try:
-    cursor.execute("SELECT * FROM History LIMIT 1")
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:  # no access to server
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:  # database error
-        print("Database does not exist")
-    else:
-        print(err)  # get other errors
+    cursor = connection.cursor(buffered=True, dictionary=True)
+    """try to get 1 result back (not all because of possible giant query)"""
+    try:
+        cursor.execute("SELECT * FROM History LIMIT 1")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:  # no access to server
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:  # database error
+            print("Database does not exist")
+        else:
+            print(err)  # get other errors
 
 """Insert query function"""
 
 
-def insertIntoDatabase(dataStatus, dataWeerstand, dataWindkracht, dataWindrichting, dataInstatie):
+def insertIntoDatabase(dataStatus, dataWaterstand, dataWindkracht, dataWindrichting, dataInstatie):
     """Prepared query"""
     add_action = ("INSERT INTO History "
                   "(Status, Waterstand, Windkracht, Windrichting, Instantie) "
@@ -30,7 +33,7 @@ def insertIntoDatabase(dataStatus, dataWeerstand, dataWindkracht, dataWindrichti
     """Bind varbiales into query"""
     data_action = {
         'status': dataStatus,
-        'waterstand': dataWeerstand,
+        'waterstand': dataWaterstand,
         'windkracht': dataWindkracht,
         'windrichting': dataWindrichting,
         'instantie': dataInstatie
@@ -58,5 +61,6 @@ def closeDatabaseConnection():
 
 #  test data 1, 75, 9, 359, 'Primair'
 """Executes the function with given data. !position == variable!"""
-insertIntoDatabase(1, 25, 3, 456, 'Primair')
-closeDatabaseConnection()
+#makeDatabaseConnection()
+#insertIntoDatabase(1, 25, 3, 456, 'Primair')
+#closeDatabaseConnection()
