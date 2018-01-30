7db6eba82233bb87521f53d13ace911ead39949c
Frank <Frank.vanKan@student.hu.nl>
2018-01-30 16:43:59 +0100

    fixes

--- serverProgram.py
+++ serverProgram.py
@@ -5,7 +5,7 @@ import GUI
 
 
 
-t1 = threading.Thread(target=doApiCall())
+t1 = threading.Thread(target=doApiCall)
 t2 = threading.Thread(target=giveInstruction(1,"192.168.42.3","192.168.42.1"))
 t3 = threading.Thread(target=GUI.runGui())
 
