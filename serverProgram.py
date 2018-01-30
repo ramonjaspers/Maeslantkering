e10bdf769adb1b9e99d5208799f2e2778a47f3f6
Frank <Frank.vanKan@student.hu.nl>
2018-01-30 16:06:40 +0100

    fixed thread order

--- serverProgram.py
+++ serverProgram.py
@@ -5,9 +5,9 @@ import GUI
 
 
 
-t1 = threading.Thread(target=GUI.runGui())
-t2 = threading.Thread(target=doApiCall())
-t3 = threading.Thread(target=giveInstruction(1,"192.168.42.3","192.168.42.1"))
+t1 = threading.Thread(target=doApiCall())
+t2 = threading.Thread(target=giveInstruction(1,"192.168.42.3","192.168.42.1"))
+t3 = threading.Thread(target=GUI.runGui())
 
 
 t1.start()
