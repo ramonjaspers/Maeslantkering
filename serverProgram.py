dfd271ea0e453513cacf070f133f991978469e83
Robert <robert.griffioen@student.hu.nl>
2018-01-30 17:01:58 +0100

    fixed merge conflict

--- serverProgram.py
+++ serverProgram.py
@@ -6,11 +6,9 @@ import GUI
 
 
 t1 = threading.Thread(target=doApiCall)
-<<<<<<< HEAD
+
 t2 = threading.Thread(target=giveInstruction, args=(1,"192.168.42.3","192.168.42.1"))
-=======
-t2 = threading.Thread(target=giveInstruction,args=(1,"192.168.42.3","192.168.42.1"))
->>>>>>> c39b1630466cedb80716bbc4240f016b3b349e26
+
 t3 = threading.Thread(target=GUI.runGui)
 
 
